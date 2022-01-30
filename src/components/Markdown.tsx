a5838f9cd44fa8b298ce3e90b14cd1a3960d6cdf
RealShadowNova <real.shadow.nova@gmail.com>
2022-01-30 17:37:00 -0500

    feat(code): add syntax highlighting

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,6 +1,11 @@
-import { Component } from 'solid-js';
+import highlight from 'highlight.js';
+import { Component, onMount } from 'solid-js';
 import Container from './Container';
 
-const Markdown: Component = (props) => <Container class="markdown">{props.children}</Container>;
+const Markdown: Component = (props) => {
+  onMount(() => highlight.highlightAll());
+
+  return <Container class="markdown">{props.children}</Container>;
+};
 
 export default Markdown;
