3e56c834cb7f2cfd864f5c3be7800702699b22a3
RealShadowNova <real.shadow.nova@gmail.com>
2022-02-02 00:08:57 -0500

    fix(sidebar): left-margin

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,7 @@
 import { useLocation, useNavigate } from 'solid-app-router';
 import { Component, createEffect, createSignal, Show } from 'solid-js';
 
-const SidebarChild: Component<{ name: string; baseURL: string; depth?: number }> = (props) => {
-  props.depth ??= 0;
-
+const SidebarChild: Component<{ name: string; baseURL: string; sub?: boolean }> = (props) => {
   const [open, setOpen] = createSignal(false);
   const { pathname } = useLocation();
 
@@ -12,12 +10,12 @@ const SidebarChild: Component<{ name: string; baseURL: string; depth?: number }>
   });
 
   return (
-    <section class="text-lg text-slate-300">
+    <section class="text-lg dark:text-slate-300">
       <div
         onClick={() => setOpen(!open())}
-        class="block cursor-pointer p-1 px- my-1 rounded-md transition ease-in-out hover:bg-neutral-200 dark:hover:bg-slate-800 duration-300"
+        class="block cursor-pointer p-1 px-4 my-1 rounded-md transition ease-in-out hover:bg-neutral-200 dark:hover:bg-slate-800 duration-300"
       >
-        <span class={pathname.startsWith(props.baseURL) ? `text-emerald-500 ml-${props.depth * 4}` : `ml-${props.depth * 4}`}>{props.name}</span>
+        <span class={`${props.sub ? 'ml-4' : ''} ${pathname.startsWith(props.baseURL) ? 'text-emerald-500' : ''}`}>{props.name}</span>
         <span class="float-right mt-1.5 text-slate-400">
           <Show
             when={open()}
@@ -40,20 +38,16 @@ const SidebarChild: Component<{ name: string; baseURL: string; depth?: number }>
   );
 };
 
-const SidebarLink: Component<{ name: string; href: string; depth?: number }> = (props) => {
-  props.depth ??= 0;
-
+const SidebarLink: Component<{ name: string; href: string; sub?: boolean }> = (props) => {
   const navigate = useNavigate();
   const { pathname } = useLocation();
 
   return (
     <span
       onClick={() => navigate(props.href)}
-      class={`block pl-4 py-1 my-1 ml-${
-        props.depth * 4 + 6
-      } rounded-md cursor-pointer transition ease-in-out hover:bg-neutral-200 dark:hover:bg-slate-800 duration-300 ${
-        pathname === props.href ? 'text-emerald-500 bg-neutral-200 dark:bg-slate-800' : ''
-      }`}
+      class={`block pl-4 py-1 my-1 rounded-md cursor-pointer transition ease-in-out hover:bg-neutral-200 dark:hover:bg-slate-800 duration-300 ${
+        props.sub ? 'ml-8' : 'ml-4'
+      } ${pathname === props.href ? 'text-emerald-500 bg-neutral-200 dark:bg-slate-800' : ''}`}
     >
       {props.name}
     </span>
@@ -62,26 +56,26 @@ const SidebarLink: Component<{ name: string; href: string; depth?: number }> = (
 
 const Sidebar: Component = (props) => (
   <div class="min-h-screen flex flex-row">
-    <div class="flex flex-col w-1/5 overflow-hidden shadow-md dark:shadow-slate-800 bg-neutral-100 dark:bg-slate-900">
+    <div class="flex flex-col w-1/4 overflow-hidden shadow-md dark:shadow-slate-800 bg-neutral-100 dark:bg-slate-900">
       <div class="py-4 px-4 dark:text-white">
         <SidebarChild name="General" baseURL="/docs/General">
-          <SidebarLink name="Welcome" href="/docs/General/Welcome" />
+          <SidebarLink name="Welcome" href="/docs/General/Welcome" sub />
         </SidebarChild>
 
         <SidebarChild name="Guide" baseURL="/docs/Guide">
-          <SidebarChild name="Getting Started" baseURL="/docs/Guide/getting-started" depth={1}>
-            <SidebarLink href="/docs/Guide/getting-started/getting-started-with-josh" name="Getting Started with Josh" />
-            <SidebarLink href="/docs/Guide/getting-started/using-providers" name="Using Providers" />
+          <SidebarChild name="Getting Started" baseURL="/docs/Guide/getting-started" sub>
+            <SidebarLink href="/docs/Guide/getting-started/getting-started-with-josh" name="Getting Started with Josh" sub />
+            <SidebarLink href="/docs/Guide/getting-started/using-providers" name="Using Providers" sub />
           </SidebarChild>
 
-          <SidebarChild name="Middleware" baseURL="/docs/Guide/middleware" depth={1}>
-            <SidebarLink href="/docs/Guide/middleware/what-is-middleware" name="What is Middleware?" />
-            <SidebarLink href="/docs/Guide/middleware/creating-middleware" name="Creating Middleware" />
+          <SidebarChild name="Middleware" baseURL="/docs/Guide/middleware" sub>
+            <SidebarLink href="/docs/Guide/middleware/what-is-middleware" name="What is Middleware?" sub />
+            <SidebarLink href="/docs/Guide/middleware/creating-middleware" name="Creating Middleware" sub />
           </SidebarChild>
         </SidebarChild>
 
         <SidebarChild name="Documentation" baseURL="/docs/Documentation">
-          <SidebarLink href="/docs/Documentation/documentation" name="Documentation" />
+          <SidebarLink href="/docs/Documentation/documentation" name="Documentation" sub />
         </SidebarChild>
       </div>
     </div>
